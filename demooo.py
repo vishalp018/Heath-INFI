8d333447657463b2d7e377a0c2706e99107d5d4c
vishal pal <128707338+vishalp018@users.noreply.github.com>
2024-04-03 18:10:52 +0530

    Update demooo.py

--- demooo.py
+++ demooo.py
@@ -151,8 +151,8 @@ if selected == 'Diabetes Prediction':
 
     # getting the input data from the user
     col1, col2, col3 = st.columns(3)
-        with col1:
-        name = st.text_input('Name')
+    with col1:
+    name = st.text_input('Name')
 
     with col2:
         Date = st.text_input('Date')
